fc64e65a8fe76c5da184df8efb0c8faf7b90592d
Urfeen <urfeens@gmail.com>
2021-07-17 21:47:45 +0300

    feat: modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -4,6 +4,43 @@ import { createPortal } from "react-dom";
 
 const Modal = ({ children, onClose: close }) => {
   const modalRoot = document.createElement('div');
+  modalRoot.id = 'modal-root';
+
+  const transitionEndHandler = e => {
+    if (e.propertyName !== "opacity" || this.state.fadeType === "in") return;
+    if (this.state.fadeType === "out") {
+      this.props.onClose();
+    }
+  };
+
+  // { fadeType: null };
+  // background = React.createRef();
+  // componentDidMount() {
+  //     window.addEventListener("keydown", this.onEscKeyDown, false);
+  //     setTimeout(() => this.setState({ fadeType: "in" }), 0);
+  //   }
+  // componentDidUpdate(prevProps, prevState) {
+  //     if (!this.props.isOpen && prevProps.isOpen) {
+  //       this.setState({ fadeType: "out" });
+  //     }
+  //   }
+  // componentWillUnmount() {
+  //     window.removeEventListener("keydown", this.onEscKeyDown, false);
+  //   }
+  // transitionEnd = e => {
+  //     if (e.propertyName !== "opacity" || this.state.fadeType === "in") return;
+  // if (this.state.fadeType === "out") {
+  //       this.props.onClose();
+  //     }
+  //   };
+  // onEscKeyDown = e => {
+  //     if (e.key !== "Escape") return;
+  //     this.setState({ fadeType: "out" });
+  //   };
+  // handleClick = e => {
+  //     e.preventDefault();
+  //     this.setState({ fadeType: "out" });
+  //   };
 
   useEffect(() => {
     document.body.appendChild(modalRoot);
@@ -14,7 +51,30 @@ const Modal = ({ children, onClose: close }) => {
 
   return (
     createPortal(
-      <div>{children}</div>,
+      <>
+        <div className="modal-box"
+        // id={this.props.id}
+        // size={this.props.size}
+        // onTransitionEnd={this.transitionEnd}
+        // fadeType={this.state.fadeType}
+        >
+          <div className="modal-box__header">
+            <h4 className="modal-box__title">Title Of Modal</h4>
+            <button onClick={typeof close === "function" ? close : ''} className="modal-box__close">
+              ×
+            </button>
+          </div>
+          <div className="modal-box__content">{children}</div>
+          <div className="modal-box__footer">
+            footer
+          </div>
+        </div>
+        <div
+          className="modal-background"
+        // onMouseDown={this.handleClick}
+        // ref={this.background}
+        />
+      </>,
       modalRoot
     )
   )
