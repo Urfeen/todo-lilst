b1d37a30d34c850daa4c77471bc31ebf565281cf
Urfeen <urfeens@gmail.com>
2021-07-28 14:59:30 +0300

    refactor: severed styles from component TextareaWithSubTextareas

---
+++ src/components/TextareaWithSubTextarea/TextareaWithSubTextarea.css.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+const StyledTextareaWithSubTextarea = styled.div`
+  textarea {
+    width: 100%;
+    resize: none;
+    padding: 4px;
+    font-size: 16px;
+    background-color: rgba(0, 0, 0, 0);
+    border: 1px #2b3044 solid;
+    color: #ccc;
+    transition: border 0.2s ease, color 0.2s ease;
+    border-radius: 3px;
+  }
+  textarea:focus {
+    border: 1px #395ac0 solid;
+    box-shadow: 0px 0px 3px 1px #395ac050;
+    color: #fff;
+  }
+  .sub-textareas {
+    display: flex;
+    &__visual-nav {
+      width: 30px;
+      polyline {
+        transition: stroke 0.2s ease;
+      }
+    }
+    &__list {
+      flex: 1 1 auto;
+      li {
+        margin: 7px 0 0 0;
+      }
+      textarea {
+        font-size: 14px;
+      }
+    }
+  }
+`;
+
+export default StyledTextareaWithSubTextarea;
--- src/components/TextareaWithSubTextarea/TextareaWithSubTextarea.jsx
+++ src/components/TextareaWithSubTextarea/TextareaWithSubTextarea.jsx
@@ -1,44 +1,7 @@
 import { nanoid } from "nanoid";
-import { useEffect, useState, useRef, useMemo } from "react";
+import { useEffect, useState } from "react";
 import TextareaAutosize from "react-textarea-autosize";
-import styled from "styled-components";
-
-const StyledTextareaWithSubTextarea = styled.div`
-  textarea {
-    width: 100%;
-    resize: none;
-    padding: 4px;
-    font-size: 16px;
-    background-color: rgba(0, 0, 0, 0);
-    border: 1px #2b3044 solid;
-    color: #ccc;
-    transition: border 0.2s ease, color 0.2s ease;
-    border-radius: 3px;
-  }
-  textarea:focus {
-    border: 1px #395ac0 solid;
-    box-shadow: 0px 0px 3px 1px #395ac050;
-    color: #fff;
-  }
-  .sub-textareas {
-    display: flex;
-    &__visual-nav {
-      width: 30px;
-      polyline {
-        transition: stroke 0.2s ease;
-      }
-    }
-    &__list {
-      flex: 1 1 auto;
-      li {
-        margin: 7px 0 0 0;
-      }
-      textarea {
-        font-size: 14px;
-      }
-    }
-  }
-`;
+import StyledTextareaWithSubTextarea from "./TextareaWithSubTextarea.css.js";
 
 const TextareaWithSubTextarea = ({
   className,
@@ -52,7 +15,7 @@ const TextareaWithSubTextarea = ({
   const [focusVisualNavIndex, setFocusVisualNavIndex] = useState(null);
 
   const _MARGIN_TOP = 7;
-  const MAX_AMOUNT_OF_LINES = 9;
+  const _MAX_AMOUNT_OF_LINES = 9;
 
   const textareaOnChangeHandler = (event) => {
     event.preventDefault();
@@ -170,7 +133,7 @@ const TextareaWithSubTextarea = ({
                 heightOfEachSubtask.length * _MARGIN_TOP
               }
             >
-              {showLines && data.subTextareas.length <= MAX_AMOUNT_OF_LINES
+              {showLines && data.subTextareas.length <= _MAX_AMOUNT_OF_LINES
                 ? data.subTextareas.map((subTextarea, index) => {
                     return (
                       <polyline
